95eb4cdc31dba4f16035327c4c5d78e311652db1
kapilesh <kapileshc@canvendor.com>
2022-12-01 00:45:38 +0530

    second commit

--- hrm_1/urls.py
+++ hrm_1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.employee import EmployeeList
+from .views.employee import EmployeeList,EmployeeDetails
 
 urlpatterns = [
-    path('employees', EmployeeList.as_view())
+    path('employees', EmployeeList.as_view()),
+    path('employees/<int:pk>',EmployeeDetails.as_view())
 ]
--- hrms/settings.py
+++ hrms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9p@=ix$q5w@(82y)68c=&)1bnjx79*$lrz#(+81(levu62n3_j'
+# SECRET_KEY = 'django-insecure-9p@=ix$q5w@(82y)68c=&)1bnjx79*$lrz#(+81(levu62n3_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
